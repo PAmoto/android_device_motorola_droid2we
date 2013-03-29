d6b2862356f16d98d72bec688e0d6b9e0bda79e8
Timothy Wong <masterchung7@gmail.com>
2013-03-28 23:26:06 -0500

    pa update

--- cm.mk
+++ cm.mk
@@ -1,18 +1,21 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Inherit PA stuff
+$(call inherit-product, vendor/pa/config/pa_common.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Boot animation
-TARGET_BOOTANIMATION_NAME := vertical-480x854
+TARGET_BOOTANIMATION_NAME := vertical-480x800
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/droid2we/droid2we.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := droid2we
-PRODUCT_NAME := cm_droid2we
+PRODUCT_NAME := pa_droid2we
 PRODUCT_BRAND := verizon
 PRODUCT_MODEL := DROID2 Global
 PRODUCT_MANUFACTURER := Motorola
